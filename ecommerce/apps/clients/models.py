5b28fdb54335df0f822f1e7961f782445501609a
JoseMORIANO <jmoriano@hacklibre.com>
2020-08-10 16:33:11 -0500

    Creacion del modelo banco y modificacion del modelo Cliente y Contacto

--- ecommerce/apps/clients/models.py
+++ ecommerce/apps/clients/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 # Create your models here.
 class Client(models.Model):
     STATUS_CHOICES = (
@@ -13,6 +14,8 @@ class Client(models.Model):
     website = models.URLField("Sitio Web", blank=True, null=True)
     email = models.EmailField("Correo", blank=True, null=True)
     type_person = models.IntegerField(choices=STATUS_CHOICES)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    RUC = models.CharField("RUC", max_length=15, default="")
     addres = models.CharField("Dirección Fiscal", max_length=255)
 
     create_at = models.DateTimeField(auto_now_add=True)
@@ -26,7 +29,9 @@ class Client(models.Model):
         return f"{self.bussines_name}"
 
 class Contact(models.Model):
+
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name=("Cliente"))
+    prefix = models.CharField("Prenombre", max_length=50, null=True, blank=True, default="")
     name = models.CharField("Nombre del Contacto", max_length=255)
     mobile_phone = PhoneNumberField("Numero del movil")
     email = models.EmailField("Correo")
@@ -44,3 +49,52 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+class Bankaccount(models.Model):
+
+    BANK_CHOICES = (
+        (1,"BCP"),
+        (2,"BBVA Continental"),
+        (3,"Interbank"),
+        (4,"BanBif"),
+        (5,"MiBanco"),
+        (6,"ScotiaBank"),
+        (7,"Banco Falabella"),
+        (8,"Banco Ripley"),
+        (9,"Banco Azteca"),
+        (10,"Banco Pichincha"),
+        (11,"Banco de Comercio"),
+        (12,"Banco Santander"),
+        (13,"Banco GNB"),
+        (14,"CRAC CAT Perú"),
+        (15,"ICBC PERU BANK"),
+    )
+
+    ACCOUNT_CHOICES = (
+        (1,"Crédito"),
+        (2,"Débito"),
+    )
+
+    CURRENCY_CHOICES = (
+        (1,"PEN"),
+        (2,"USD"),
+    )
+
+    STATUS_CHOICES = (
+        (1,"Activo"),
+        (2,"Inactivo"),
+        (3,"Otros"),
+    )
+
+    bank_id = models.IntegerField("Banco", choices=BANK_CHOICES)
+    type_account = models.IntegerField("Tipo de Cuenta", choices=ACCOUNT_CHOICES)
+    currency_id = models.IntegerField("Tipo de moneda", choices=CURRENCY_CHOICES)
+    status = models.IntegerField("Estado", choices=STATUS_CHOICES)
+    CCI_number = models.IntegerField("Cuenta Interbancaria", max_length=20)
+
+    class Meta:
+        verbose_name = 'Banco'
+        verbose_name_plural = 'Bancos'
+
+    def __str__(self):
+        return f"{self.bank_id}"
